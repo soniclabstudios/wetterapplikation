4bcd5530d893198cc91d1eb3392cc486e9d710dd
Andi Jud <info@soniclab.ch>
2014-05-23 11:22:10 +0200

    Temperatur und Windgeschwindigkeit einbauen

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,25 +1,20 @@
-var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-};
-
 function success(pos) {
       var crd = pos.coords;
 
       jQuery(".longitude").text(crd.longitude);
       jQuery(".latitude").text(crd.latitude);
-      jQuery(".accuracy").text(crd.accuracy + ' meters.');
+      jQuery(".accuracy").text(crd.accuracy + ' m');
 
 
       jQuery.ajax({
-          url: 'https://api.forecast.io/forecast/2440fc192add591a5ce89da2c8939529/' + crd.latitude + ',' + crd.longitude,
+          url: 'https://api.forecast.io/forecast/a4f100023b169ab8debbbd8f37eedb85/' + crd.latitude + ',' + crd.longitude,
           data: {
               units: 'si'
           },
           dataType: 'jsonp',
           success: function(data) {
-                jQuery(".temperature").text(data.currently.apparentTemperature);
+                jQuery(".temperature").text(data.currently.apparentTemperature + ' °C');
+                jQuery(".windspeed").text(data.currently.windSpeed + ' m/h');
               console.log(data);
           }
       });

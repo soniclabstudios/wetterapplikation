67c18fec6955a5b3504e8621450800c74aee60e6
Andi Jud <info@soniclab.ch>
2014-06-06 09:09:15 +0200

    weffler js abgekupfert

--- javascripts/script.js
+++ javascripts/script.js
@@ -4,41 +4,59 @@ var options = {
   maximumAge: 0
 };
 
+
 function success(pos) {
-    var crd = pos.coords;
+  var crd = pos.coords;
 
-    $('.js-lat').text(crd.latitude);
-    $('.js-long').text(crd.longitude);
-    $('.js-acc').text(crd.accuracy + ' m');
 
-    $.ajax({
-        url: 'https://api.forecast.io/forecast/a955df0e9afe8c822ebb3adf30265fb6/' + crd.latitude + ',' + crd.longitude,
-        data: {
-            units : 'si'
-        },
-        dataType: 'jsonp',
-        success: function(data) {
-            console.log(data);
-            $('.js-temp').text(data.currently.apparentTemperature + ' °C');
-            $('.js-windspeed').text(data.currently.windSpeed + ' m/s');
-        }
-    });
+  console.log('Your current position is:');
+  console.log('Latitude : ' + crd.latitude);
+  console.log('Longitude: ' + crd.longitude);
+  console.log('More or less ' + crd.accuracy + ' meters.');
 
-    $.ajax({
-        url: 'https://maps.googleapis.com/maps/api/geocode/json',
-        data: {
-            latlng: crd.latitude + ',' + crd.longitude,
-            sensor: true
-        },
-        success: function(data) {
-            console.log(data);
-            $('.js-address').text(data.results[0].formatted_address);
-        }
-    });
-}
+  $('.js-lat').text(crd.latitude);
+  $('.js-long').text(crd.longitude);
+  $('.js-acc').text(crd.accuracy +'m');
+
+
+  $.ajax({
+    url: 'https://api.forecast.io/forecast/4cbf11a0b6a5166782b8d4cb9d5defef/' + crd.latitude + ',' + crd.longitude,
+    data: {
+        units: 'si'
+    },
+    dataType: 'jsonp',
+    success: function(data) {
+        $('.js-temp').text(data.currently.apparentTemperature + '°C');
+        $('.js-windsp').text(data.currently.windSpeed + 'm/s');
+    }
+
+  });
+
+  $.ajax({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json',
+    data: {
+        latlng: crd.latitude + ',' + crd.longitude,
+        sensor: true
+    },
+    success: function(data) {
+        $('.js-loc').text(data.results[0].formatted_address);
+      console.log(data);
+
+    }
+
+  });
+
+  var output = document.getElementById("out");
+  var img = new Image();
+  img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + crd.latitude + "," + crd.longitude + "&zoom=13&size=400x400&sensor=true";
+
+  output.appendChild(img);
+
+
+};
 
 function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
-}
+};
 
 navigator.geolocation.getCurrentPosition(success, error, options);
